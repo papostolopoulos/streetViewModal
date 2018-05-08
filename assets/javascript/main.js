7dde7fe93dbf7a9da5cf564b2029547a467d224a
Paraskevas Apostolopoulos <papost@fb.com>
2018-05-07 17:07:07 -0700

    working image display with coordinates entry

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,21 +4,56 @@ var buttonCoordinates = document.getElementById("buttonCoordinates");
 var inputCoordinates = document.getElementById("inputCoordinates");
 var divIdModal = document.getElementById("divIdModal");
 var spanIdModalClose = document.getElementById("spanIdModalClose");
+var imgIdModalPhoto = document.getElementById("imgIdModalPhoto");
+var imageAttributes = {
+  lat: 40.720032,
+  lon: -73.988354,
+  fov: 90,
+  heading: 235,
+  pitch: 10
+};
+var imageSource = `https://maps.googleapis.com/maps/api/streetview?size=800x800
+&location=${imageAttributes.lat},${imageAttributes.lon}
+&fov=${imageAttributes.fov}
+&heading=${imageAttributes.heading}
+&pitch=${imageAttributes.pitch}`;
 
-//Display the modal when clicking on the button
+//DISPLAY MODAL ON CLICK OF SUBMIT BUTTON
 buttonCoordinates.addEventListener("click", function() {
-  var valueCoordinates = inputCoordinates.value;
+  var coordinatesArr = inputCoordinates.value.includes(",") ?
+  inputCoordinates.value.split(",") : inputCoordinates.value.includes(", ") ?
+  inputCoordinates.value.split(", ") :
+  inputCoordinates.value.split(" ");
+  imageAttributes.lat = coordinatesArr[0];
+  imageAttributes.lon = coordinatesArr[1];
+
+
+  imgIdModalPhoto.src = `https://maps.googleapis.com/maps/api/streetview?size=800x800
+  &location=${imageAttributes.lat},${imageAttributes.lon}
+  &fov=${imageAttributes.fov}
+  &heading=${imageAttributes.heading}
+  &pitch=${imageAttributes.pitch}`;
   divIdModal.style.display = "block";
+  console.log(imgIdModalPhoto.src);
 });
 
-//Close the modal window when clicking around it
-window.addEventListener("click", function() {
-  if (event.target === divIdModal) {
-    divIdModal.style.display = "none";
-  }
-});
 
-//Close the modal window when clicking on the x
-spanIdModalClose.addEventListener("click", function() {
-  divIdModal.style.display = "none";
-});
+//CLOSE MODAL WINDOW WHEN CLICKING AROUND CONTENT
+window.onclick = ()=>{
+  if (event.target === divIdModal) divIdModal.style.display = "none";
+};
+// window.addEventListener("click", function() {
+//   if (event.target === divIdModal) {
+//     divIdModal.style.display = "none";
+//   }
+// });
+
+
+//CLOSE MODAL WINDOW WHEN CLICKING ON X
+spanIdModalClose.onclick = () => divIdModal.style.display = "none";
+// spanIdModalClose.addEventListener("click", function() {
+//   divIdModal.style.display = "none";
+// });
+
+/*https://maps.googleapis.com/maps/api/streetview?size=800x800&location=40.720032,-73.988354
+&fov=90&heading=235&pitch=10 */
--- todo.md
+++ todo.md
@@ -1,1 +1,19 @@
 # streetViewModal
+
+* create html core [X]
+* create css core [X]
+* create modal effect [X]
+* Put the X on top of image [X]
+* Get icons for the scrolling and zoom effect [X]
+* Put CSS for form and for page H1 []
+* Modify padding and positioning of icons []
+* Put js conditions with error alert for the right text entry in the input box []
+
+
+
+* create js core [X]
+* Manage different ways of entering coordinates []
+* Update the image according to the entry of the user []
+* Update the angle of the Street View according to the icon clicks []
+* Update the angle of the street View according to the keyboard presses []
+* Update the zoom according to the mouse scrolling []
